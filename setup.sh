795f2b7e563e2cc5662e9a8409465decc4aa85b4
David Tao <ruoyutao@gmail.com>
2018-07-23 13:35:38 -0400

    working setup for mac

--- setup.sh
+++ setup.sh
@@ -3,6 +3,14 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT license.
 
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
 
 set -ex
 echo "Running setup.sh...";
@@ -16,6 +24,10 @@ cd textworld/thirdparty/
 if [ ! -e I7_6M62_Linux_all.tar.gz ]; then
     echo "Downloading Inform7 CLI"
     curl -LO http://inform7.com/download/content/6M62/I7_6M62_Linux_all.tar.gz
+    if [ "${machine}" == 'Mac' ] && [ ! -e I7-6M62-OSX.dmg ]; then
+        echo "Downloading Inform7 for Mac"
+        curl -LO http://inform7.com/download/content/6M62/I7-6M62-OSX.dmg
+    fi
 fi
 if [ ! -d inform7-6M62 ]; then
     tar xf I7_6M62_Linux_all.tar.gz
@@ -25,6 +37,24 @@ fi
     cd inform7-6M62/
     ./install-inform7.sh --prefix $PWD
 )
+
+# Mount DMG if we're using a Mac
+if [ "${machine}" == 'Mac' ]; then
+    echo "Mounting Inform for Mac"
+    hdiutil attach ./I7-6M62-OSX.dmg
+
+    echo "Copying Mac compiled inform files"
+    current_dir="`pwd`"
+    cd /Volumes/Inform/Inform.app/Contents/MacOS
+    /bin/cp -t "$current_dir/share/inform7/Compilers/" cBlorb inform6 Inform intest ni
+    /bin/cp ./git "$current_dir/share/inform7/Interpreters/dumb-git"
+    /bin/cp ./glulxe "$current_dir/share/inform7/Interpreters/dumb-glulxe"
+
+    cd "$current_dir"
+
+    echo "Unmounting Inform for Mac"
+    hdiutil detach /Volumes/Inform/
+fi
 
 # Install a modified fork of Frotz and build it specifically for interaction with the command line
 if [ ! -d frotz ]; then
