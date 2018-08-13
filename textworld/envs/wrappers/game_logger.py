f8de962fe85d499a29e9b66adc82564e08c2488c
Ruo Yu Tao <t-rutao@microsoft.com>
2018-08-13 16:02:53 -0400

    first iteration of game logger

--- textworld/envs/wrappers/game_logger.py
+++ textworld/envs/wrappers/game_logger.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Tuple
+from typing import Tuple, List
 
 from textworld.core import Environment, GameState, Wrapper
 
@@ -12,10 +12,33 @@ class GameLogger(Wrapper):
         Parameters
         ----------
         :param env:
-            The TextWorld environment to wrap
+            The TextWorld environment to wrap. Has the correct knowledge base.
         """
         super().__init__(env)
         self.activate_state_tracking()
 
+        self.serialized_game = env
+
+        self.logs = []
+        self.current_log = {}
+
     def step(self, command: str) -> Tuple[GameState, float, bool]:
+        if self.current_log:
+            self.logs.append(self.current_log)
+            self.current_log = {}
+
+        self.current_log['action_taken'] = command
+
         game_state, score, done = super().step(command)
+        return game_state, score, done
+
+    def log_action_distribution(self, actions: List, probabilities: List):
+        action_dist = {a: p for a, p in zip(actions, probabilities)}
+        self.current_log['action_distribution'] = action_dist
+
+        return self.logs
+
+    def log(self, to_log):
+        self.current_log['others'] = to_log
+
+        return self.logs[:].append(self.current_log)
