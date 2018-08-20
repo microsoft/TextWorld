a4ca1433a42c4e0dd7eb6a419048698212fe6822
David Tao <t-rutao@microsoft.com>
2018-08-20 17:53:13 -0400

    untested game logger

--- textworld/envs/wrappers/glulx_logger.py
+++ textworld/envs/wrappers/glulx_logger.py
@@ -5,6 +5,7 @@ from typing import Tuple, List, Optional, Iterable, Union, Sized, Any, Mapping
 
 from textworld.core import Environment, GameState, Wrapper
 from textworld.envs.glulx.git_glulx_ml import GitGlulxMLEnvironment, GlulxGameState
+from textworld.envs.wrappers import GameLog
 
 
 class GlulxLogger(Wrapper):
@@ -12,10 +13,8 @@ class GlulxLogger(Wrapper):
         """
         Wrap around a TextWorld GitGlulxML environment to provide logging capabilities.
 
-        Parameters
-        ----------
-        :param env:
-            The GitGlulxML environment to wrap.
+        Args:
+            env: The GitGlulxML environment to wrap.
         """
         super().__init__(env)
         self.activate_state_tracking()
@@ -23,17 +22,21 @@ class GlulxLogger(Wrapper):
         self.serialized_game = env.game.serialize()
         self._gamefile = env.gamefile
 
+        self._logs = GameLog()
+        self._current_log = self._logs.current_game
+        self._current_log.append({})
+        self._current = self._current_log[-1]
+
 
     def step(self, command: str) -> Tuple[GlulxGameState, float, bool]:
         """
-        Take a step in the environment, save needed information.
-        :param command:
-            input string for taking an action
-        :return:
+        Take a step in the environment.
+        Args:
+            command: input string for taking an action
+
+        Returns:
             GlulxGameState, score and done.
         """
-        self._logs.append(self._current)
-        self._current = {'optional': []}
 
         self._current['command'] = command
 
@@ -49,8 +52,11 @@ class GlulxLogger(Wrapper):
     def reset(self) -> GameState:
         """
         Reset the environment.
-        Also clears logs.
+        Adds a new game into the logs.
+        Returns:
+            GameState
         """
+        # if not self._current
         self._logs = []
         game_state = super().reset()
         self._current = {'optional': []}
@@ -62,65 +68,92 @@ class GlulxLogger(Wrapper):
     def add_commands(self, commands: List[str], scores: Optional[Union[Iterable[float], Sized]]=None) -> None:
         """
         Add custom commands to the logger. Optionally add scores for each command.
-        :param commands:
-            A list of commands.
-        :param scores:
-            scores for each command. Must be same size as commands if provided.
-        :return:
-        """
-        command_mapping = commands
-        if scores is not None:
-            assert len(scores) == len(commands)
-            command_mapping = {a: p for a, p in zip(commands, scores)}
+        Args:
+            commands: A list of commands.
+            scores: scores for each command. Must be same size as commands if provided.
 
-        self._current['command_distribution'] = command_mapping
+        Returns:
+
+        """
+        if scores is not None:
+            self._current['command_scores'] = scores
+
+        self._current['commands'] = commands
 
     def add(self, info: Any) -> None:
         """
         Add any additional information you want to log.
-        :param info:
-            Additional information to log for the current game state.
+        Args:
+            info: Additional information to log for the current game state.
         """
         self._current['optional'].append(info)
 
     @property
     def current(self) -> Mapping:
+        """
+        Returns:
+            Current game state logs.
+        """
         return self._current
 
     @property
     def logs(self) -> List[Mapping]:
         """
-        Get all logs
-        :return: List of all logs
+        Returns: List of all logs from this game.
         """
-        logs = self._logs[:]
-        logs.append(self._current)
-        return logs
+        return self._current_log
 
     @property
-    def gamefile(self):
+    def all_logs(self) -> GameLog:
+        """
+        Returns: GameLog object containing all logs.
+        """
+        return self._logs
+
+    @property
+    def gamefile(self) -> str:
+        """
+        Returns:
+            Game file currently loaded
+        """
         return self._gamefile
 
     def __getitem__(self, index: int) -> Mapping:
         """
         Get a certain log at a given index.
-        :param index:
-            index of log to get.
-        :return:
-            log at index.
+        Args:
+            index: index of log to get.
+        Returns:
+            log at index
         """
         assert index <= len(self._logs)
 
-        if index < len(self._logs) - 1:
-            return self._logs[index]
         return self._current
 
     def __str__(self) -> str:
-        return str(self.logs)
+        return str(self._current_log)
 
     def serialize(self) -> List[Mapping]:
         """
         Get serialized mappings of logs.
-        :return: List of serialized mappings.
+        Returns:
+            List of serialized mappings.
         """
-        return self.logs
+        return self._logs.logs
+
+    def save(self, filename) -> None:
+        """
+        Saves all logs given a filename
+        Returns: None
+        """
+        self._logs.save(filename)
+
+    def load(self, filename) -> None:
+        """
+        Loads logs from a file
+        Args:
+            filename:
+                string representing file location
+        Returns: None
+        """
+        self._logs.load(filename)
