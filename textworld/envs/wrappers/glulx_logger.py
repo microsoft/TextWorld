5c498d5d6df9312d81d6172337aa6938577ee862
David Tao <t-rutao@microsoft.com>
2018-08-21 10:11:06 -0400

    fix requested changes

--- textworld/envs/wrappers/glulx_logger.py
+++ textworld/envs/wrappers/glulx_logger.py
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT license.
+
 import json
 
 from typing import Tuple, List, Optional, Iterable, Union, Sized, Any, Mapping
@@ -9,20 +10,21 @@ from textworld.envs.glulx.git_glulx_ml import GitGlulxMLEnvironment, GlulxGameSt
 
 
 class GameLog:
+    """
+    GameLog object. Allows you to load and save previous game logs.
+    """
     def __init__(self):
-        """
-        GameLog object. Allows your to load and save previous game logs.
-        """
         self._logs = [[]]
-        self._current_game = self._logs[-1]
         self._filename = ''
 
     def __getitem__(self, idx: int) -> list:
-        """
+        """A
         Gets a particular game log at index idx.
+
         Args:
             idx: index to retrieve
-        Returns:
+
+        Returns: list of logs at idx
 
         """
         assert idx <= len(self._logs)
@@ -35,14 +37,16 @@ class GameLog:
     def current_game(self) -> list:
         """
         Gets current game we're logging.
+
         Returns: list of logs from current game.
         """
-        return self._current_game
+        return self._logs[-1]
 
     @property
     def logs(self) -> list:
         """
         Get all logs from all games.
+
         Returns: All logs from all games.
         """
         return self._logs
@@ -50,32 +54,34 @@ class GameLog:
     def new_game(self):
         """
         Start logs for a new game.
+
         Returns: log object for current game.
         """
-        if len(self._current_game) > 0:
+        if len(self.current_game) > 0:
             self._logs.append([])
-        self._current_game = self._logs[-1]
-        return self._current_game
+        return self.current_game
 
     def set(self, key: Any, value: Any) -> None:
         """
         Sets value for latest game
+
         Args:
             key: Key to set
             value: Value to set
 
         """
-        current = self._current_game[-1]
+        current = self.current_game[-1]
         current[key] = value
 
     def append_optional(self, value: Any) -> None:
         """
         Appends optional information to current game
+
         Args:
-            value: Value to append
+            value: Value to append. Must be JSON serializable.
 
         """
-        current = self._current_game[-1]
+        current = self.current_game[-1]
         if 'optional' not in current:
             current['optional'] = []
         current['optional'].append(value)
@@ -83,15 +89,17 @@ class GameLog:
     def add_log(self, log: Mapping):
         """
         Adds a new log to our logs
+
         Args:
             log: Mapping of a log
 
         """
-        self._current_game.append(log)
+        self.current_game.append(log)
 
     def save(self, filename):
         """
         Save current logs to specified file name
+
         Args:
             filename: File path to save to (should have JSON extension)
 
@@ -106,6 +114,7 @@ class GameLog:
     def load(self, filename):
         """
         Loads a JSON object as logs
+
         Args:
             filename: file path to load.
 
@@ -116,13 +125,13 @@ class GameLog:
 
 
 class GlulxLogger(Wrapper):
-    def __init__(self, env: GitGlulxMLEnvironment) -> None:
-        """
-        Wrap around a TextWorld GitGlulxML environment to provide logging capabilities.
+    """
+    Wrap around a TextWorld GitGlulxML environment to provide logging capabilities.
 
-        Args:
-            env: The GitGlulxML environment to wrap.
-        """
+    Args:
+        env: The GitGlulxML environment to wrap.
+    """
+    def __init__(self, env: GitGlulxMLEnvironment) -> None:
         super().__init__(env)
         self.activate_state_tracking()
 
@@ -134,8 +143,10 @@ class GlulxLogger(Wrapper):
     def step(self, command: str) -> Tuple[GlulxGameState, float, bool]:
         """
         Take a step in the environment.
+
         Args:
             command: input string for taking an action
+
         Returns:
             GlulxGameState, score and done.
         """
@@ -158,6 +169,7 @@ class GlulxLogger(Wrapper):
         """
         Reset the environment.
         Adds a new game into the logs.
+
         Returns:
             GameState
         """
@@ -177,6 +189,7 @@ class GlulxLogger(Wrapper):
     def add_commands(self, commands: List[str], scores: Optional[Union[Iterable[float], Sized]]=None) -> None:
         """
         Add custom commands to the logger. Optionally add scores for each command.
+
         Args:
             commands: A list of commands.
             scores: scores for each command. Must be same size as commands if provided.
@@ -190,6 +203,7 @@ class GlulxLogger(Wrapper):
     def add(self, info: Any) -> None:
         """
         Add any additional information you want to log.
+
         Args:
             info: Additional information to log for the current game state.
         """
@@ -228,8 +242,10 @@ class GlulxLogger(Wrapper):
     def __getitem__(self, index: int) -> Mapping:
         """
         Get a certain log at a given index.
+
         Args:
             index: index of log to get.
+
         Returns:
             log at index
         """
@@ -243,6 +259,7 @@ class GlulxLogger(Wrapper):
     def serialize(self) -> List[Mapping]:
         """
         Get serialized mappings of logs.
+
         Returns:
             List of serialized mappings.
         """
@@ -251,6 +268,7 @@ class GlulxLogger(Wrapper):
     def save(self, filename) -> None:
         """
         Saves all logs given a filename
+
         Returns: None
         """
         self._logs.save(filename)
@@ -258,9 +276,11 @@ class GlulxLogger(Wrapper):
     def load(self, filename) -> None:
         """
         Loads logs from a file
+
         Args:
             filename:
                 string representing file location
+
         Returns: None
         """
         self._logs.load(filename)
