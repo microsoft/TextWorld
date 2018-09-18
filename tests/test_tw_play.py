d19826bd7d280ee7d73a02020185c2751638ca00
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2018-09-18 15:03:00 -0400

    BF: random modes were calling the wrong classes

---
+++ tests/test_tw_play.py
@@ -0,0 +1,21 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+
+from subprocess import check_call
+
+import textworld
+from textworld.utils import make_temp_directory
+
+
+def test_making_a_custom_game():                                             
+    with make_temp_directory(prefix="test_tw-play") as tmpdir:    
+        game_file, _ = textworld.make(5, 10, 5, {}, seed=1234, games_dir=tmpdir)
+
+        command = ["tw-play", "--max-steps", "100", "--mode", "random", game_file]
+        assert check_call(command) == 0
+
+        command = ["tw-play", "--max-steps", "100", "--mode", "random-cmd", game_file]
+        assert check_call(command) == 0
+
+        command = ["tw-play", "--max-steps", "100", "--mode", "walkthrough", game_file]
+        assert check_call(command) == 0
