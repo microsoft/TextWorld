324be812efda83941570df1abec9fae9101cc130
Egor Bulychev <egorbu@gmail.com>
2018-10-18 23:19:50 +0200

    Update setup.py
    * add a long description for example https://github.com/Microsoft/jupyter-Kqlmagic/blob/master/setup.py
    * use codecs to read files to avoid crashes during the installation on machines with non-standard locales
    * make quotes consistent

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@
 # Licensed under the MIT license.
 
 
+import codecs
 import os
 
 from setuptools import setup, find_packages
@@ -21,14 +22,14 @@ def _pre_install(dir):
 class CustomInstall(install):
     def run(self):
         self.execute(_pre_install, (self.install_lib,),
-                     msg="Running post install task")
+                     msg='Running post install task')
         install.run(self)
 
 
 class CustomDevelop(develop):
     def run(self):
         self.execute(_pre_install, (self.install_lib,),
-                     msg="Running post install task")
+                     msg='Running post install task')
         develop.run(self)
 
 
@@ -36,6 +37,16 @@ class CustomBuildPy(build_py):
     def run(self):
         _pre_install(None)
         build_py.run(self)
+
+
+# Get the long description from the README file
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    # This happens when running tests
+    long_description = None
 
 
 setup(
@@ -50,17 +61,18 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     scripts=[
-        "scripts/tw-data",
-        "scripts/tw-play",
-        "scripts/tw-make",
-        "scripts/tw-stats",
+        'scripts/tw-data',
+        'scripts/tw-play',
+        'scripts/tw-make',
+        'scripts/tw-stats',
     ],
     license='',
     zip_safe=False,
-    description="Microsoft Textworld - A Text-based Learning Environment.",
-    cffi_modules=["glk_build.py:ffibuilder"],
+    description='Microsoft Textworld - A Text-based Learning Environment.',
+    long_description=long_description,
+    cffi_modules=['glk_build.py:ffibuilder'],
     setup_requires=['cffi>=1.0.0'],
-    install_requires=open('requirements.txt').readlines(),
+    install_requires=codecs.open('requirements.txt', encoding='utf-8').readlines(),
     test_suite='nose.collector',
     tests_require=[
         'nose==1.3.7',
