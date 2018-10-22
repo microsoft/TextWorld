6849ecea4384ba9fcbe9565747678937676ec694
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2018-10-22 10:27:47 -0400

    Remove extras in run-* scripts

--- docker/run-tests.sh
+++ docker/run-tests.sh
@@ -4,6 +4,6 @@ set -ex
 
 export PATH=$PWD/glulx/Git-Glulx:$PATH
 
-pip3 install -v .[prompt,vis]
+pip3 install -v .
 pip3 install nose coverage
 nosetests -sv --with-xunit --with-coverage --cover-xml --cover-html --cover-package textworld
