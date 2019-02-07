733e80bda9842223568d6a12b861258e2540265a
Tavian Barnes <tavian.barnes@microsoft.com>
2019-02-07 16:34:54 -0500

    package.sh: New script to build all the distributed packages

---
+++ package-impl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Based on https://github.com/pypa/python-manylinux-demo/blob/master/travis/build-wheels.sh
+
+set -e
+
+cd /usr/src/TextWorld
+
+for PYTHON in /opt/python/cp3[5-7]*/bin/python; do
+    $PYTHON setup.py bdist_wheel -d wheelhouse
+done
+
+for WHEEL in wheelhouse/textworld-*.whl; do
+    auditwheel repair $WHEEL -w dist
+done
