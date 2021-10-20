5bafc9f0a14984b29754217b4c27ebb2af7b8640
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2021-10-20 09:26:24 -0400

    Update notebooks.

--- notebooks/make_games.sh
+++ notebooks/make_games.sh
@@ -1,7 +1,7 @@
-tw-make tw-simple --rewards dense    --goal detailed --seed 18 --test --silent -f --output games/rewardsDense_goalDetailed.ulx
-tw-make tw-simple --rewards balanced --goal detailed --seed 18 --test --silent -f --output games/rewardsBalanced_goalDetailed.ulx
-tw-make tw-simple --rewards sparse   --goal detailed --seed 18 --test --silent -f --output games/rewardsSparse_goalDetailed.ulx
-tw-make tw-simple --rewards dense    --goal brief    --seed 18 --test --silent -f --output games/rewardsDense_goalBrief.ulx
-tw-make tw-simple --rewards balanced --goal brief    --seed 18 --test --silent -f --output games/rewardsBalanced_goalBrief.ulx
-tw-make tw-simple --rewards sparse   --goal brief    --seed 18 --test --silent -f --output games/rewardsSparse_goalBrief.ulx
-tw-make tw-simple --rewards sparse   --goal none     --seed 18 --test --silent -f --output games/rewardsSparse_goalNone.ulx
+tw-make tw-simple --rewards dense    --goal detailed --seed 18 --test --silent -f --output games/tw-rewardsDense_goalDetailed.z8
+tw-make tw-simple --rewards balanced --goal detailed --seed 18 --test --silent -f --output games/tw-rewardsBalanced_goalDetailed.z8
+tw-make tw-simple --rewards sparse   --goal detailed --seed 18 --test --silent -f --output games/tw-rewardsSparse_goalDetailed.z8
+tw-make tw-simple --rewards dense    --goal brief    --seed 18 --test --silent -f --output games/tw-rewardsDense_goalBrief.z8
+tw-make tw-simple --rewards balanced --goal brief    --seed 18 --test --silent -f --output games/tw-rewardsBalanced_goalBrief.z8
+tw-make tw-simple --rewards sparse   --goal brief    --seed 18 --test --silent -f --output games/tw-rewardsSparse_goalBrief.z8
+tw-make tw-simple --rewards sparse   --goal none     --seed 18 --test --silent -f --output games/tw-rewardsSparse_goalNone.z8
