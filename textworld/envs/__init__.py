d70ea887a68a6a129408d1c0246f61e881484b4c
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2023-11-21 07:28:35 -0500

    Add helper function to load the right Environment based on file extension

--- textworld/envs/__init__.py
+++ textworld/envs/__init__.py
@@ -1,7 +1,19 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT license.
+import re
 
 from textworld.envs.glulx.git_glulx import GitGlulxEnv
 from textworld.envs.zmachine.jericho import JerichoEnv
 from textworld.envs.tw import TextWorldEnv
 from textworld.envs.wrappers.tw_inform7 import TWInform7
+
+
+def _guess_backend(path):
+    # Guess the backend from the extension.
+    if path.endswith(".ulx"):
+        return GitGlulxEnv
+    elif re.search(r"\.z[1-8]", path):
+        return JerichoEnv
+
+    msg = "Unsupported game format: {}".format(path)
+    raise ValueError(msg)
