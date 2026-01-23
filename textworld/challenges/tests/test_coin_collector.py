e5388ef706573e3dd3c951d4dec2c509101a26b9
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2026-01-23 12:16:58 -0500

    Limit nb rooms in coin collector

--- textworld/challenges/tests/test_coin_collector.py
+++ textworld/challenges/tests/test_coin_collector.py
@@ -15,13 +15,18 @@ def test_making_coin_collector():
         101: {"quest_length": 1, "nb_rooms": 2},
         200: {"quest_length": 100, "nb_rooms": 200},
         201: {"quest_length": 1, "nb_rooms": 3},
-        # 300: {"quest_length": 100, "nb_rooms": 300},  # Too large for .z8 game file.
+        300: {"quest_length": 100, "nb_rooms": 300},
     }
     for level in [1, 100, 101, 200, 201, 300]:
         options = textworld.GameOptions()
         options.seeds = 1234
 
         settings = {"level": level}
+        if expected[level]["nb_rooms"] > 100:
+            with pytest.raises(ValueError):
+                coin_collector.make(settings, options)
+            continue
+
         game = coin_collector.make(settings, options)
         assert len(game.quests[0].commands) == expected[level]["quest_length"]
         assert len(game.world.rooms) == expected[level]["nb_rooms"]
